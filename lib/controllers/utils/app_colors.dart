43e3af2240075caec14bd7979dfe58fc5e0edc1b
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-04-07 14:47:41 +0600

    app color updated

---
+++ lib/controllers/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0XFF2D3250);
+  static const Color appBarColor = Color(0XFF363062);
+  static const Color buttonColorInactive = Color(0XFF31363F);
+  static const Color buttonColorActive = Color(0XFF008170);
+  static const Color textColor1 = Color(0XFFFAF0E6);
+  static const Color textColor2 = Color(0XFF5C5470);
+  static const Color borderColor = Color(0XFF435585);
+  static const Color cardColor = Color(0XFF363062);
+  static const Color primaryColor = Color(0XFF2D3250);
+  static const Color secondaryColor = Color(0XFF092635);
+}
